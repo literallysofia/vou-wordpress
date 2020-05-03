f993df949545cc8be730cb933df45654b95e3607
Sofia Silva <up201505628@fe.up.pt>
2020-05-03 22:51:06 +0100

    minor fix in departments

--- src/template-parts/dep-right.php
+++ src/template-parts/dep-right.php
@@ -8,15 +8,21 @@
  * @since VOU 1.0
  */
 
-$color = sanitize_hex_color(get_field('wp_dep_color'));
+$color = sanitize_hex_color(get_field('wp_project_color'));
 $style = "box-shadow: 25px -25px 0px 0px " .  $color . ";";
-$email = sanitize_email(get_field('wp_dep_email'));
+$email = sanitize_email(get_field('wp_project_email'));
+$icon = get_field('wp_project_icon');
 ?>
 
 <div class="single-dep right">
     <div class="row">
         <div class="col-sm-12 col-md-6">
-            <h3><?php the_title(); ?></h3>
+            <div class="heading d-flex align-items-center justify-content-end">
+                <?php if ($icon) : ?>
+                    <img src="<?php echo esc_url($icon) ?>" alt="<?php echo get_the_title() . ' icon' ?>" class="dep-icon" />
+                    <h3><?php the_title(); ?></h3>
+                <?php endif; ?>
+            </div>
             <div class="content">
                 <?php the_content(); ?>
             </div>
