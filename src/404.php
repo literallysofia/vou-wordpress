30ce787a673b768df81a4eb83c8c753a39b5c608
Sofia Silva <up201505628@fe.up.pt>
2020-05-14 22:58:03 +0100

    404 page

---
+++ src/404.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package WordPress
+ * @subpackage VOU
+ * @since VOU 1.0
+ */
+
+get_header();
+?>
+
+<main class="page">
+    <div class="container-fluid">
+        <div class="heading">
+            <h2>Woops!</h2>
+            <h1><?php echo 'Página não encontrada' ?></h1>
+        </div>
+        <p>Desculpa, mas a página que estás à procura não existe, foi removida, alterou de nome ou está temporariamente indisponível.</p>
+        <button type="button" class="btn btn-primary mt-5">Voltar ao Início</button>
+    </div>
+</main>
+
+<?php get_footer(); ?>
